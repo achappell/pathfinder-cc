e823706588e2a30e2727d94ccd52f02821af05d8
Amanda Chappell <achappell@me.com>
2014-05-09 18:58:16 -0500

    stop ignoring pods folder, for now

---
+++ Pods/Pods-PathfinderCCTests-OCMock-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PathfinderCCTests_OCMock : NSObject
+@end
+@implementation PodsDummy_Pods_PathfinderCCTests_OCMock
+@end
---
+++ Pods/Pods-PathfinderCCTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PathfinderCCTests : NSObject
+@end
+@implementation PodsDummy_Pods_PathfinderCCTests
+@end
